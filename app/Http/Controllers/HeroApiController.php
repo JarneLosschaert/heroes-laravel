69b6179ac9a5d10f07e28894671a84758ffb19f0
Loesje <jarne.losschaert@student.howest.be>
2023-03-31 16:00:58 +0200

    added hero controller

--- app/Http/Controllers/HeroApiController.php
+++ app/Http/Controllers/HeroApiController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Hero;
+use App\Modules\Heroes\Services\HeroService;
 use Illuminate\Http\Request;
 
 class HeroApiController extends Controller
 {
-    //
+    private $_service;
+
+    public function __construct(HeroService $service)
+    {
+        $this->_service = $service;
+    }
+
+    public function all(Request $request)
+    {
+        $pages = $request->get("pages", 10);
+        return $this->_service->all($pages);
+    }
+
+    public function find($id)
+    {
+        $data = $this->_service->find($id);
+        return ["data" => $data];
+    }
+
+    public function create(Request $request)
+    {
+        $data = $request->all();
+        $movie = $this->_service->create($data);
+        
+        if ($this->_service->hasErrors()) {
+            return ["errors" => $this->_service->getErrors()];
+        }
+        
+        return ["data" => $movie];
+    }
+
 }
