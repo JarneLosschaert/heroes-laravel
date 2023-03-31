df46b932bcba8a62114016382d0d655fef4a79be
Loesje <jarne.losschaert@student.howest.be>
2023-03-31 16:07:53 +0200

    added routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/heroes', [MovieApiController::class, 'all']);
+Route::get('/heroes/{id}', [MovieApiController::class, 'find']);
+
+Route::post('/movies', [MovieApiController::class, 'create']);
+
