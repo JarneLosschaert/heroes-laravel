bfd63e7ed1ba2a7e43e8893d413de82412aea0e0
Loesje <jarne.losschaert@student.howest.be>
2023-04-27 21:53:21 +0200

    api routes added

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/heroes', [HeroApiController::class, 'all']);
+Route::get('/heroes/list', [HeroApiController::class, 'list']);
 Route::get('/heroes/{id}', [HeroApiController::class, 'find']);
-
 Route::post('/heroes', [HeroApiController::class, 'create']);
+Route::put('/heroes/{id}', [HeroApiController::class, 'update']);
+Route::delete('/heroes/{id}', [HeroApiController::class, 'delete']);
+
+
+Route::get('/users', [UserApiController::class, 'all']);
+Route::get('/users/{id}', [UserApiController::class, 'find']);
+Route::post('/users', [UserApiController::class, 'create']);
+Route::patch('/users/{id}', [UserApiController::class, 'update']);
+Route::delete('/users/{id}', [UserApiController::class, 'delete']);
