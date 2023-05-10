de77864b390dd96b8b7d707172c55e1fe07e4364
jarne.losschaert <jarne.losschaert@student.howest.be>
2023-05-10 11:26:32 +0000

    Revert "Merge conflict opgelost"
    This reverts commit f22ae391dda89f30a8e1f0cc563ad6d57fe970ac

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\HeroApiController;
+use App\Http\Controllers\UserApiController;
+use Illuminate\Routing\RouteAction;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,15 +21,16 @@ Route::get('/', function () {
 });
 
 Route::get('/heroes', [HeroApiController::class, 'all']);
+Route::get('/heroes/list', [HeroApiController::class, 'list']);
 Route::get('/heroes/{id}', [HeroApiController::class, 'find']);
-
 Route::post('/heroes', [HeroApiController::class, 'create']);
 Route::put('/heroes/{id}', [HeroApiController::class, 'update']);
 Route::delete('/heroes/{id}', [HeroApiController::class, 'delete']);
 
+
 Route::get('/users', [UserApiController::class, 'all']);
 Route::get('/users/{id}', [UserApiController::class, 'find']);
-
 Route::post('/users', [UserApiController::class, 'create']);
-Route::put('/users/{id}', [UserApiController::class, 'update']);
+Route::patch('/users/{id}', [UserApiController::class, 'update']);
+Route::delete('/users/{id}', [UserApiController::class, 'delete']);
 
